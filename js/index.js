524132caceb1892657eb729de5858f41e1344293
farhanacsebd <farhana.cse.bd@gmail.com>
2023-01-29 19:39:06 +0600

    change the bg color using js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const btnRed =document.getElementById('btnRed');
 const btnWhite =document.getElementById('btnWhite');
 const btnBlack =document.getElementById('btnBlack');
 const addImg = document.getElementById('addImg');
-
+console.log(addImg.parentElement.parentElement.children[0]);
 btnRed.addEventListener('click',(e)=>{
     e.preventDefault()
     addImg.textContent = '';
@@ -11,6 +11,8 @@ btnRed.addEventListener('click',(e)=>{
     `
     <img src="/images/coke001.png" alt="">
     `;
+    addImg.parentElement.parentElement.style.backgroundColor = '#161616';
+    addImg.parentElement.parentElement.children[0].style.backgroundColor = '#830C0F';
     addImg.appendChild(div);
 });
 
@@ -22,6 +24,8 @@ btnWhite.addEventListener('click',(e)=>{
     `
     <img src="/images/coke002.png" alt="">
     `;
+    addImg.parentElement.parentElement.style.backgroundColor = '#A1A19F';
+    addImg.parentElement.parentElement.children[0].style.backgroundColor = '#D10B20';
     addImg.appendChild(div);
 });
 
@@ -33,5 +37,7 @@ btnBlack.addEventListener('click',(e)=>{
     `
     <img src="/images/coke003.png" alt="">
     `;
+    addImg.parentElement.parentElement.style.backgroundColor = '#830C0F';
+    addImg.parentElement.parentElement.children[0].style.backgroundColor = '#090D0B';
     addImg.appendChild(div);
 });
